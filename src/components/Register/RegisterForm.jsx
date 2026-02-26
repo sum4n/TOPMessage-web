108810850012ccbf47b6ff1c81585c05a3607173
Suman Das <sum4ndas@gmail.com>
2026-02-26 11:25:32 +0530

    feat(component): add and style RegisterForm component

---
+++ src/components/Register/RegisterForm.jsx
@@ -0,0 +1,32 @@
+import styles from "./RegisterForm.module.css";
+
+function RegisterForm({ loginLink }) {
+  return (
+    <div className={styles.registerContainer}>
+      <h1>Register</h1>
+      <form action="#" method="post" className={styles.registerForm}>
+        <div className={styles.formLabel}>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className={styles.formLabel}>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" name="password" />
+        </div>
+        <div className={styles.formLabel}>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input type="text" id="confirmPassword" name="confirmPassword" />
+        </div>
+        <button type="submit" className={styles.registerButton}>
+          Register
+        </button>
+      </form>
+
+      <p>
+        Already have an account? <a href={loginLink}>Login</a>
+      </p>
+    </div>
+  );
+}
+
+export default RegisterForm;
