4303ac114d8343ed2d03521e608b24b58eae441f
Suman Das <sum4ndas@gmail.com>
2026-02-26 11:13:33 +0530

    feat(component): create and style Login form component

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,28 @@
+import styles from "./Login.module.css";
+
+function Login({ registerLink }) {
+  return (
+    <div className={styles.loginContainer}>
+      <h1>Log In</h1>
+      <form action="#" method="post" className={styles.loginForm}>
+        <div className={styles.formLabel}>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className={styles.formLabel}>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" name="password" />
+        </div>
+        <button type="submit" className={styles.loginButton}>
+          Log in
+        </button>
+      </form>
+
+      <p>
+        New user? <a href={registerLink}>Register</a>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
