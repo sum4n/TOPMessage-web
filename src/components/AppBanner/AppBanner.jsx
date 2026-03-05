dc4c1e113c5da47b08b13eb79a23f76682812024
Suman Das <sum4ndas@gmail.com>
2026-03-05 20:34:56 +0530

    feat(component): add AppBanner and style it

---
+++ src/components/AppBanner/AppBanner.jsx
@@ -0,0 +1,11 @@
+import styles from "./AppBanner.module.css";
+
+function AppBanner() {
+  return (
+    <div className={styles.bannerContainer}>
+      <h1>Welcome to my Messaging App</h1>
+    </div>
+  );
+}
+
+export default AppBanner;
