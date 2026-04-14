1e151a0e9e2a977e8253fa6c164088fc1dd29c41
Suman Das <sum4ndas@gmail.com>
2026-04-14 19:07:25 +0530

    feat(component): fetch user data from api

--- src/components/UsersAndGroups/UsersAndGroups.jsx
+++ src/components/UsersAndGroups/UsersAndGroups.jsx
@@ -4,6 +4,16 @@ import { useEffect, useState } from "react";
 function UsersAndGroups() {
   const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.users);
+        setUsers(response.users);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className={styles.userAndGrpContainer}>
       Users and groups
