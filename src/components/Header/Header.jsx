2d38a8bde24a9e2129fe221fa4dfd105a074873f
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-08-04 22:38:49 +0530

    feat: add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "../UserHeader/UserHeader.module.css";
+
+function UserHeader() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>TOP Message</h1>
+      </header>
+    </>
+  );
+}
+
+export default UserHeader;
