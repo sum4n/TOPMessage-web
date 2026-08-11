38b1d1893fb42b66efd593e0350d35c87ba10c27
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-08-11 08:11:03 +0530

    feat(auth): wrap App inside protected route

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,19 @@ import App from "./App.jsx";
 import LoginForm from "./components/LoginForm/LoginForm.jsx";
 import Register from "./components/Register/RegisterForm.jsx";
 import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <ProtectedRoute />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+    ],
   },
   {
     path: "login",
