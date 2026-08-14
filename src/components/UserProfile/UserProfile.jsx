5890c6e7e512c0305c0a70004f2f9c3683dd9ae5
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-08-14 07:41:59 +0530

    feat: update profile name and display errors

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,13 @@
-function UserProfile({ profile }) {
+import { useState } from "react";
+
+function UserProfile({
+  profile,
+  updateProfile,
+  profileUpdating,
+  profileUpdateError,
+}) {
+  const [name, setName] = useState(profile.name || "");
+
   return (
     <>
       <div>
@@ -7,6 +16,24 @@ function UserProfile({ profile }) {
         <hr />
         <p>Name:</p>
         <p>{profile.name || "Set your name"}</p>
+        <form onSubmit={(e) => updateProfile(e, name)}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Update name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" disabled={profileUpdating}>
+            Update name
+          </button>
+        </form>
+        {profileUpdateError && (
+          <ul>
+            {profileUpdateError.map((error) => {
+              return <li key={error.msg}>{error.msg}</li>;
+            })}
+          </ul>
+        )}
         <hr />
         <p>Joined at:</p>
         <p>{profile.joinedAt}</p>
